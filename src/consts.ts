45db2159e1156ed2c9f7e189518ca6660ea54d8d
Romit Mohane <69901221+Reckadon@users.noreply.github.com>
2025-06-19 02:21:17 +0530

    updated site desc

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Romit Mohane';
-export const SITE_DESCRIPTION = 'Personal Portfolio Website'; // site meta description for SEO
+export const SITE_DESCRIPTION = "Romit's Personal Space on the Internet"; // site meta description for SEO
